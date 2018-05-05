3dc203a208ce661f40bc4077c8952b3532073bfc
Alina Zare <zarea@missouri.edu>
2018-05-05 14:14:29 -0400

    remove unixp

--- unmixP_NCM/unmixP_NCM_Parameters.m
+++
@@ -1,6 +0,0 @@
-function [Parameters] = unmixP_NCM_Parameters()
-% This is the parameter setting part for sample NCM proportion estimation
-
-Parameters.NumberIterations = 1000; % number of iterations
-
-end
